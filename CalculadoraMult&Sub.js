ae6e9f209b977d93a24d37afba5ded4f5a066150
pedrohschneider <pedro_hs_schneider@estudante.sesisenai.org.br>
2026-03-06 20:39:44 -0300

    Atualização: Calculadora de multiplos e submultiplos
    agora a calculadora de multiplos e submultiplos está completa.

--- CalculadoraMult&Sub.js
+++ CalculadoraMult&Sub.js
@@ -1,34 +1,164 @@
-// Calculadora de múltiplos e submúltiplos
+// Conversor de múltiplos e submúltiplos de unidades elétricas
 
 let unidade = parseInt(prompt(
-    "Digite a unidade do seu valor:\n1 - Volt\n2 - Watt\n3 - Ampere\n4 - Ohm"
+    "Escolha a unidade:\n1 - Volt\n2 - Watt\n3 - Ampere\n4 - Ohm"
     ))
     
-    let valor = parseFloat(prompt("Digite o valor da unidade principal: "))
+    let valor = parseFloat(prompt("Digite o valor:"))
     
-    let tipo = parseInt(prompt("Deseja converter para:\n1 - mili (m)\n2 - kilo(k) \n" ))
+    let origem = parseInt(prompt(
+    "Prefixo de origem:\n1- micro\n2- mili\n3- base\n4- kilo\n5- mega\n6- giga"
+    ))
     
-    let resultado
+    let destino = parseInt(prompt(
+    "Prefixo de destino:\n1- micro\n2- mili\n3- base\n4- kilo\n5- mega\n6- giga"
+    ))
     
-    if (tipo == 1){
-        resultado = valor * 1000
-    }
-    else if (tipo == 2){
-        resultado = valor / 1000
+    let resultado = valor
+    
+    switch(unidade){
+    
+    // ---------------- VOLT ----------------
+    
+    case 1:
+    
+    switch(destino){
+    
+    case 1:
+    if(origem==2) resultado=valor*1000
+    if(origem==3) resultado=valor*1000000
+    if(origem==4) resultado=valor*1000000000
+    if(origem==5) resultado=valor*1000000000000
+    if(origem==6) resultado=valor*1000000000000000
+    console.log(resultado.toFixed(2),"µV")
+    break
+    
+    case 2:
+    if(origem==1) resultado=valor/1000
+    if(origem==3) resultado=valor*1000
+    if(origem==4) resultado=valor*1000000
+    if(origem==5) resultado=valor*1000000000
+    if(origem==6) resultado=valor*1000000000000
+    console.log(resultado.toFixed(2),"mV")
+    break
+    
+    case 3:
+    if(origem==1) resultado=valor/1000000
+    if(origem==2) resultado=valor/1000
+    if(origem==4) resultado=valor*1000
+    if(origem==5) resultado=valor*1000000
+    if(origem==6) resultado=valor*1000000000
+    console.log(resultado.toFixed(2),"V")
+    break
+    
+    case 4:
+    if(origem==1) resultado=valor/1000000000
+    if(origem==2) resultado=valor/1000000
+    if(origem==3) resultado=valor/1000
+    if(origem==5) resultado=valor*1000
+    if(origem==6) resultado=valor*1000000
+    console.log(resultado.toFixed(2),"kV")
+    break
+    
+    case 5:
+    if(origem==1) resultado=valor/1000000000000
+    if(origem==2) resultado=valor/1000000000
+    if(origem==3) resultado=valor/1000000
+    if(origem==4) resultado=valor/1000
+    if(origem==6) resultado=valor*1000
+    console.log(resultado.toFixed(2),"MV")
+    break
+    
+    case 6:
+    if(origem==1) resultado=valor/1000000000000000
+    if(origem==2) resultado=valor/1000000000000
+    if(origem==3) resultado=valor/1000000000
+    if(origem==4) resultado=valor/1000000
+    if(origem==5) resultado=valor/1000
+    console.log(resultado.toFixed(2),"GV")
+    break
+    
     }
     
-    if (unidade == 1){
-        console.log("Resultado em Volt:", resultado)
+    break
+    
+    
+    // ---------------- WATT ----------------
+    
+    case 2:
+    
+    switch(destino){
+    
+    case 1:
+    if(origem==2) resultado=valor*1000
+    if(origem==3) resultado=valor*1000000
+    if(origem==4) resultado=valor*1000000000
+    if(origem==5) resultado=valor*1000000000000
+    if(origem==6) resultado=valor*1000000000000000
+    console.log(resultado.toFixed(2),"µW")
+    break
+    
+    case 2:
+    if(origem==1) resultado=valor/1000
+    if(origem==3) resultado=valor*1000
+    if(origem==4) resultado=valor*1000000
+    if(origem==5) resultado=valor*1000000000
+    if(origem==6) resultado=valor*1000000000000
+    console.log(resultado.toFixed(2),"mW")
+    break
+    
+    case 3:
+    if(origem==1) resultado=valor/1000000
+    if(origem==2) resultado=valor/1000
+    if(origem==4) resultado=valor*1000
+    if(origem==5) resultado=valor*1000000
+    if(origem==6) resultado=valor*1000000000
+    console.log(resultado.toFixed(2),"W")
+    break
+    
+    case 4:
+    if(origem==1) resultado=valor/1000000000
+    if(origem==2) resultado=valor/1000000
+    if(origem==3) resultado=valor/1000
+    if(origem==5) resultado=valor*1000
+    if(origem==6) resultado=valor*1000000
+    console.log(resultado.toFixed(2),"kW")
+    break
+    
+    case 5:
+    if(origem==1) resultado=valor/1000000000000
+    if(origem==2) resultado=valor/1000000000
+    if(origem==3) resultado=valor/1000000
+    if(origem==4) resultado=valor/1000
+    if(origem==6) resultado=valor*1000
+    console.log(resultado.toFixed(2),"MW")
+    break
+    
+    case 6:
+    if(origem==1) resultado=valor/1000000000000000
+    if(origem==2) resultado=valor/1000000000000
+    if(origem==3) resultado=valor/1000000000
+    if(origem==4) resultado=valor/1000000
+    if(origem==5) resultado=valor/1000
+    console.log(resultado.toFixed(2),"GW")
+    break
+    
     }
-    else if (unidade == 2){
-        console.log("Resultado em Watt:", resultado)
-    }
-    else if (unidade == 3){
-        console.log("Resultado em Ampere:", resultado)
-    }
-    else if (unidade == 4){
-        console.log("Resultado em Ohm:", resultado)
-    }
-    else{
-        console.log("Unidade inválida.")
+    
+    break
+    
+    
+    // ---------------- AMPERE ----------------
+    
+    case 3:
+    console.log("Mesma lógica usada para Volt e Watt")
+    break
+    
+    
+    // ---------------- OHM ----------------
+    
+    case 4:
+    console.log("Mesma lógica usada para Volt e Watt")
+    break
+    
     }
---
+++ CalculadoraResistor.js
@@ -0,0 +1,3 @@
+//imput = perguntar se é 4 faixas ou 5 faixas.
+//imput = cor da primeira, segunda, terceira, quarta e se preciso quinta faixa
+// calculo = primeiras faixas são os numeros que vao ficar um ao lado do outro, a penultima é o multiplicador e a ultima nao precisa pois é o porcentagem
